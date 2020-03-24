e21ced7cff5c14234ef2c3a63cb6438f6376a9dd
MB-teacher <MB-teacher@192.168.1.109>
2020-03-24 20:33:46 +0800

    Java

---
+++ src/main/java/com/study/d01/BMI.java
@@ -0,0 +1,11 @@
+package com.study.d01;
+
+public class BMI {
+    double h; // 身高
+    double w; // 體重
+    
+    double getBMI() { // 取得BMI(計算BMI數值)
+        double bmi = w / Math.pow(h/100, 2);
+        return bmi;
+    }
+}
---
+++ src/main/java/com/study/d01/BMIDemo.java
@@ -0,0 +1,7 @@
+package com.study.d01;
+
+public class BMIDemo {
+    public static void main(String[] args) {
+        
+    }
+}
