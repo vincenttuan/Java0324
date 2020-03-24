e56eee120da0e9d0c1568f5f9101a0f34eae9cba
MB-teacher <MB-teacher@192.168.1.109>
2020-03-24 20:00:15 +0800

    Java

--- src/main/java/com/study/d01/PetStoreDog.java
+++ src/main/java/com/study/d01/PetStoreDog.java
@@ -1,5 +1,7 @@
 package com.study.d01;
 
+import java.util.stream.Stream;
+
 public class PetStoreDog {
     public static void main(String[] args) {
         // 我有 2 隻狗
@@ -18,7 +20,7 @@ public class PetStoreDog {
         System.out.printf("我有一隻 %s 色的 %s 價格是 %,d\n", dog2.color, dog2.name, dog2.price);
         dog2.skill();
         
-        // 分析
+        // 分析 Java 7 (命令式)
         Dog[] dogs = {dog1, dog2};
         int sum = 0;
         for(int i=0;i<dogs.length;i++) {
@@ -26,6 +28,14 @@ public class PetStoreDog {
             sum += dogs[i].price;
         }
         System.out.printf("總價: %,d  均價: %,d\n", sum, sum/dogs.length);
+        System.out.println("Java 8 -------------------------------------------");
+        // 分析 Java 8 (聲明式)
+        // 建立串流 stream
+        // 分析串流資料
+        // 利用 forEach 印出所有資料
+        Stream.of(dogs)
+                .filter(d -> d.price < 40000)
+                .forEach(d -> System.out.printf("%s 價格 %,d\n", d.name, d.price));
         
     }
 }
