0b1799f73155b308d3efcfc7061dfac721fe6b27
MB-teacher <MB-teacher@192.168.1.109>
2020-03-26 20:52:35 +0800

    Java

---
+++ src/main/java/com/study/d02/WalletGame.java
@@ -0,0 +1,13 @@
+package com.study.d02;
+
+public class WalletGame {
+    public static void main(String[] args) {
+        WalletGame game = new WalletGame();
+        game.play();
+    }
+    
+    void play() {
+        
+    }
+    
+}
