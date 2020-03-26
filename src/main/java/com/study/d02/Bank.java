97fe55bfd24599d48494e978809005107b008e54
MB-teacher <MB-teacher@192.168.1.109>
2020-03-26 19:24:14 +0800

    Java

--- src/main/java/com/study/d02/Bank.java
+++ src/main/java/com/study/d02/Bank.java
@@ -1,12 +1,16 @@
 package com.study.d02;
 
 public class Bank {
-    String name = "中國信託"; // 銀行名稱
+    static String name = "中國信託"; // 銀行名稱
     int account; // 銀行帳號(五碼)
     int balance; // 帳戶餘額(你的錢)
     
     void printBalance() { // 印出你的帳戶資料
         System.out.printf("%s 帳號: %05d 餘額: $%,d\n", name, account, balance);
+    }
+    
+    static void printName() {
+        System.out.printf("銀行名稱: %s\n", name);
     }
     
 }
--- src/main/java/com/study/d02/BankDemo.java
+++ src/main/java/com/study/d02/BankDemo.java
@@ -2,7 +2,18 @@ package com.study.d02;
 
 public class BankDemo {
     public static void main(String[] args) {
+        Bank.name = "國泰世華"; // 類別變數
+        
         Bank b1 = new Bank();
+        b1.account = 123;
+        b1.balance = 54000;
+        
+        Bank b2 = new Bank();
+        b2.account = 567;
+        b2.balance = 48000;
+        
+        Bank.printName();
         b1.printBalance();
+        b2.printBalance();
     }
 }
