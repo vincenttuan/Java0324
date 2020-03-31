e3b44ce3125b158864883aa9b9b7146d97ef5ac5
MB-teacher <MB-teacher@192.168.1.109>
2020-03-31 19:49:01 +0800

    Java

--- src/main/java/com/study/d03/array/ArrayDemo2.java
+++ src/main/java/com/study/d03/array/ArrayDemo2.java
@@ -2,9 +2,11 @@ package com.study.d03.array;
 
 public class ArrayDemo2 {
     public static void main(String[] args) {
-        int[] i = new int[]{5, 6, 7, 8, 9};
+        int[] i = {5, 6, 7, 8, 9};
         System.out.println(i.length);
         System.out.println(i[0]);
         System.out.println(i[i.length-1]);
+        i = new int[]{1, 2, 3};
+        System.out.println(i[0]);
     }
 }
