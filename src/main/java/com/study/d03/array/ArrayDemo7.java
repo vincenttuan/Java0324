0245d9f96116ca495f7f59f6f846daf0ac89ec1c
MB-teacher <MB-teacher@192.168.1.109>
2020-04-02 18:53:15 +0800

    Java

---
+++ src/main/java/com/study/d03/array/ArrayDemo7.java
@@ -0,0 +1,27 @@
+package com.study.d03.array;
+
+import java.util.stream.IntStream;
+
+public class ArrayDemo7 {
+    public static void main(String[] args) {
+        int[] scores = {100, 80, 90};
+        // 1.1 for-loop
+        for(int i=0;i<scores.length;i++) {
+            System.out.print(scores[i] + ", ");
+        }
+        System.out.println();
+        // 1.2 for-loop
+        for(int i=0, lens=scores.length;i<lens;i++) {
+            System.out.print(scores[i] + ", ");
+        }
+        System.out.println();
+        // 2. for-each(in)
+        for(int x : scores){
+            System.out.print(x + ", ");
+        }
+        System.out.println();
+        // 3. Java8 Stream
+        IntStream.of(scores).forEach(x -> System.out.print(x + ", "));
+        System.out.println();
+    }
+}
