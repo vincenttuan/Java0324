f37af1b98a34ed6ff75c4320bffa4e364eb9bab4
MB-teacher <MB-teacher@192.168.1.109>
2020-04-07 19:10:12 +0800

    Java

--- src/main/java/com/study/d05/TryDemo2.java
+++ src/main/java/com/study/d05/TryDemo2.java
@@ -6,6 +6,10 @@ public class TryDemo2 {
     }
     public static void calc(int x, int y) {
         int sum = 0;
+        if(y == 0) {
+            ArithmeticException e = new ArithmeticException("分母不可為0");
+            throw e;
+        }
         sum = x / y;
         System.out.printf("sum= %d\n", sum);
     }
