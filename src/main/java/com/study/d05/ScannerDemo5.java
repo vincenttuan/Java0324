40a75ad3855c5b2035944befa7fe871de5eb0466
MB-teacher <MB-teacher@192.168.1.109>
2020-04-07 20:01:52 +0800

    Java

--- src/main/java/com/study/d05/ScannerDemo5.java
+++ src/main/java/com/study/d05/ScannerDemo5.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ScannerDemo5 {
     public static void main(String[] args) throws Exception {
         File f = new File("src\\main\\java\\com\\study\\d05\\myfile.txt");
-        Scanner sc = new Scanner(f).useDelimiter("\\A"); // 全部抓
+        Scanner sc = new Scanner(f, "UTF-8").useDelimiter("\\A"); // 全部抓
         System.out.println(sc.next());
     }
 }
