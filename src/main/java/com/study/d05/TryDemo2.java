70a70577f0af8aed3c5d7646494b566bfbb2319d
MB-teacher <MB-teacher@192.168.1.109>
2020-04-07 19:08:43 +0800

    Java

--- src/main/java/com/study/d05/TryDemo2.java
+++ src/main/java/com/study/d05/TryDemo2.java
@@ -1,16 +1,12 @@
 package com.study.d05;
 
-public class TryDemo {
+public class TryDemo2 {
     public static void main(String[] args) {
         calc(10, 0);
     }
     public static void calc(int x, int y) {
         int sum = 0;
-        try {
-            sum = x / y;
-        } catch (Exception e) {
-            System.out.printf("分母不可為0, 錯誤訊息是: %s\n", e);
-        }        
+        sum = x / y;
         System.out.printf("sum= %d\n", sum);
     }
 }
