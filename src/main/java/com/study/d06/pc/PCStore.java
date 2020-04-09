288abbe19ad7d361c869dd1ce7c58df9adffaf74
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 19:52:07 +0800

    Java

---
+++ src/main/java/com/study/d06/pc/PCStore.java
@@ -0,0 +1,12 @@
+package com.study.d06.pc;
+
+public class PCStore {
+    public static void main(String[] args) {
+        CPU cpu = new CPU(2000, 8000);
+        Memory memory = new Memory(32, 3000);
+        HardDriver hd = new HardDriver(1000, 5000);
+        Computer computer = new Computer(cpu, memory, hd);
+        System.out.printf("一台配備有:\n%s\n%s\n%s\n價格 %,d\n",
+                cpu, memory, hd, computer.getPrice());
+    }
+}
