664a866eb21512a90b3e97f916d5e439a14bd4d9
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 21:01:16 +0800

    Java

---
+++ src/main/java/com/study/d06/youbike/Youbike.java
@@ -0,0 +1,147 @@
+package com.study.d06.youbike;
+
+public class Youbike {
+    private int _id;
+    private String sno;
+    private String sna;
+    private String tot;
+    private String sbi;
+    private String sarea;
+    private String mday;
+    private String lat;
+    private String lng;
+    private String ar;
+    private String sareaen;
+    private String snaen;
+    private String aren;
+    private String bemp;
+    private String act;
+
+    public int getId() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+
+    public String getSno() {
+        return sno;
+    }
+
+    public void setSno(String sno) {
+        this.sno = sno;
+    }
+
+    public String getSna() {
+        return sna;
+    }
+
+    public void setSna(String sna) {
+        this.sna = sna;
+    }
+
+    public String getTot() {
+        return tot;
+    }
+
+    public void setTot(String tot) {
+        this.tot = tot;
+    }
+
+    public String getSbi() {
+        return sbi;
+    }
+
+    public void setSbi(String sbi) {
+        this.sbi = sbi;
+    }
+
+    public String getSarea() {
+        return sarea;
+    }
+
+    public void setSarea(String sarea) {
+        this.sarea = sarea;
+    }
+
+    public String getMday() {
+        return mday;
+    }
+
+    public void setMday(String mday) {
+        this.mday = mday;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getAr() {
+        return ar;
+    }
+
+    public void setAr(String ar) {
+        this.ar = ar;
+    }
+
+    public String getSareaen() {
+        return sareaen;
+    }
+
+    public void setSareaen(String sareaen) {
+        this.sareaen = sareaen;
+    }
+
+    public String getSnaen() {
+        return snaen;
+    }
+
+    public void setSnaen(String snaen) {
+        this.snaen = snaen;
+    }
+
+    public String getAren() {
+        return aren;
+    }
+
+    public void setAren(String aren) {
+        this.aren = aren;
+    }
+
+    public String getBemp() {
+        return bemp;
+    }
+
+    public void setBemp(String bemp) {
+        this.bemp = bemp;
+    }
+
+    public String getAct() {
+        return act;
+    }
+
+    public void setAct(String act) {
+        this.act = act;
+    }
+
+    @Override
+    public String toString() {
+        return "Youbike{" + "_id=" + _id + ", sno=" + sno + ", sna=" + sna + ", tot=" + tot + ", sbi=" + sbi + ", sarea=" + sarea + ", mday=" + mday + ", lat=" + lat + ", lng=" + lng + ", ar=" + ar + ", sareaen=" + sareaen + ", snaen=" + snaen + ", aren=" + aren + ", bemp=" + bemp + ", act=" + act + '}';
+    }
+    
+    
+    
+}
