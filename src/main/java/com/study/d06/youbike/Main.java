e7ff78373e38efe7dc863938f3995f5bd063a756
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 21:07:30 +0800

    Java

---
+++ src/main/java/com/study/d06/youbike/Main.java
@@ -0,0 +1,18 @@
+package com.study.d06.youbike;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String url = "https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json";
+        Reader reader = new InputStreamReader(new URL(url).openStream(), "UTF-8");
+        JsonElement je = JsonParser.parseReader(reader);
+        String json = je.toString();
+        System.out.println(json);
+        
+    }
+}
