a1f8968f3af50a103863c5249c4f061db1cad722
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 20:55:24 +0800

    Java

--- src/main/java/com/study/d06/food/Main.java
+++ src/main/java/com/study/d06/food/Main.java
@@ -13,9 +13,15 @@ public class Main {
         String url = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
         Reader reader = new InputStreamReader(new URL(url).openStream(), "UTF-8");
         JsonElement je = JsonParser.parseReader(reader);
-        System.out.println(je.toString());
+        //System.out.println(je.toString());
         // 將 json 資料轉進 RiceFailure[] 陣列
         RiceFailure[] rfs = new Gson().fromJson(je.toString(), RiceFailure[].class);
-        Stream.of(rfs).forEach(System.out::println);
+        //Stream.of(rfs).forEach(System.out::println);
+        // 查詢特定品名
+        Stream.of(rfs)
+                //.filter(r -> r.get品名().contains("日本"))
+                .filter(r -> r.getTitle().contains("108"))
+                .forEach(System.out::println);
+        
     }
 }
