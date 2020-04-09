f89d1da288c668d80118f4ecf6ad1f9d59cabfb7
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 19:00:28 +0800

    Java

---
+++ src/main/java/com/study/d06/Car.java
@@ -0,0 +1,34 @@
+package com.study.d06;
+
+public class Car {
+    private Engine engine;
+    private SteeringWheel steeringWheel;
+    private Wheel[] wheels;
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public SteeringWheel getSteeringWheel() {
+        return steeringWheel;
+    }
+
+    public void setSteeringWheel(SteeringWheel steeringWheel) {
+        this.steeringWheel = steeringWheel;
+    }
+
+    public Wheel[] getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(Wheel[] wheels) {
+        this.wheels = wheels;
+    }
+    
+    
+    
+}
