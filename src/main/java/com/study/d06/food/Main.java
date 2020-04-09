f08b76cf3193cbc74c22e675de44a9a3e8b96ba0
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 20:41:17 +0800

    Java

---
+++ src/main/java/com/study/d06/food/Main.java
@@ -0,0 +1,16 @@
+package com.study.d06.food;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String url = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        Reader reader = new InputStreamReader(new URL("").openStream(), "UTF-8");
+        JsonElement je = JsonParser.parseReader(reader);
+        
+    }
+}
