25fd02c80144c2be374930fc78a782fb9b811768
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 21:17:39 +0800

    Java

--- src/main/java/com/study/d06/youbike/Main.java
+++ src/main/java/com/study/d06/youbike/Main.java
@@ -1,10 +1,12 @@
 package com.study.d06.youbike;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,7 +14,13 @@ public class Main {
         Reader reader = new InputStreamReader(new URL(url).openStream(), "UTF-8");
         JsonElement je = JsonParser.parseReader(reader);
         String json = je.getAsJsonObject().getAsJsonObject("result").getAsJsonArray("records").toString();
-        System.out.println(json);
+        //System.out.println(json);
+        Youbike[] youbikes = new Gson().fromJson(json, Youbike[].class);
+        
+        Stream.of(youbikes)
+                .filter(y -> Integer.parseInt(y.getSbi()) >= 20)
+                .filter(y -> Integer.parseInt(y.getBemp()) >= 20)
+                .forEach(System.out::println);
         
     }
 }
