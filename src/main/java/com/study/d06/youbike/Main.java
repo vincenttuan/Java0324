a790c8b3e322691eae6a1fcd635665ed5ce9ec05
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 21:12:46 +0800

    Java

--- src/main/java/com/study/d06/youbike/Main.java
+++ src/main/java/com/study/d06/youbike/Main.java
@@ -8,10 +8,10 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String url = "https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json";
+        String url = "https://data.tycg.gov.tw/api/v1/rest/datastore/a1b4714b-3b75-4ff8-a8f2-cc377e4eaa0f?format=json&limit=400";
         Reader reader = new InputStreamReader(new URL(url).openStream(), "UTF-8");
         JsonElement je = JsonParser.parseReader(reader);
-        String json = je.toString();
+        String json = je.getAsJsonObject().getAsJsonObject("result").getAsJsonArray("records").toString();
         System.out.println(json);
         
     }
