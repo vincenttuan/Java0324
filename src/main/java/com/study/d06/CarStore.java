1935813c11291396a0e62b0047ead4d610bfc19c
MB-teacher <MB-teacher@192.168.1.109>
2020-04-09 19:04:01 +0800

    Java

--- src/main/java/com/study/d06/CarStore.java
+++ src/main/java/com/study/d06/CarStore.java
@@ -2,6 +2,12 @@ package com.study.d06;
 
 public class CarStore {
     public static void main(String[] args) {
+        Engine engine = new Engine();
+        engine.setName("V6");
+        engine.setPower(6000);
+        engine.setPower(50_0000);
+        
         Car car = new Car();
+        System.out.println(car.getEngine());
     }
 }
