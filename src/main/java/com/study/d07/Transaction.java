89e3b891bf6688d666ac4b704bd62745450093e8
MB-teacher <MB-teacher@192.168.1.109>
2020-04-14 21:23:05 +0800

    Java

--- src/main/java/com/study/d07/Transaction.java
+++ src/main/java/com/study/d07/Transaction.java
@@ -5,6 +5,11 @@ public class Transaction {
     void sumbit(int money){
         System.out.printf("現金 $%d 結帳\n", money);
     }
+    void sumbit(int money1, int money2, int money3){
+        System.out.printf("現金1 $%d 結帳\n", money1);
+        System.out.printf("現金2 $%d 結帳\n", money2);
+        System.out.printf("現金3 $%d 結帳\n", money3);
+    }
     void sumbit(String cardNo){
         System.out.printf("Yoyo 卡號: $%s 結帳\n", cardNo);
     }
@@ -20,5 +25,6 @@ public class Transaction {
         t.sumbit(10.5);
         t.sumbit("111-222-333-444");
         t.sumbit(100);
+        t.sumbit(100, 200, 300);
     }
 }
