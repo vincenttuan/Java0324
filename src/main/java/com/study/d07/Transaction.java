1d79529913f80df204a58de64d741e58253b6082
MB-teacher <MB-teacher@192.168.1.109>
2020-04-14 21:28:40 +0800

    Java

--- src/main/java/com/study/d07/Transaction.java
+++ src/main/java/com/study/d07/Transaction.java
@@ -1,15 +1,32 @@
 package com.study.d07;
 
 // 超載(Overloading)練習
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Transaction {
     void sumbit(int money){
         System.out.printf("現金 $%d 結帳\n", money);
     }
     void sumbit(int money1, int money2, int money3){
+        int total = money1 + money2 + money3;
         System.out.printf("現金1 $%d 結帳\n", money1);
         System.out.printf("現金2 $%d 結帳\n", money2);
         System.out.printf("現金3 $%d 結帳\n", money3);
+        System.out.println("---------------------");
+        System.out.printf("小計 $%d \n", total);
     }
+    
+    void submit(int[] money) {
+        int total = IntStream.of(money).sum();
+        IntStream.of(money).forEach(m -> {
+            System.out.printf("現金 $%d 結帳\n", m);
+        });
+        System.out.println("---------------------");
+        System.out.printf("小計 $%d \n", total);
+    }
+    
     void sumbit(String cardNo){
         System.out.printf("Yoyo 卡號: $%s 結帳\n", cardNo);
     }
@@ -26,5 +43,7 @@ public class Transaction {
         t.sumbit("111-222-333-444");
         t.sumbit(100);
         t.sumbit(100, 200, 300);
+        int[] money = {100, 200, 300, 400, 500, 600};
+        t.submit(money);
     }
 }
