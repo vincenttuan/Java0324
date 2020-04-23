7df60724eede3cf28dd1caec831b6fb4db0aa71e
MB-teacher <MB-teacher@192.168.1.109>
2020-04-23 19:36:06 +0800

    Java

--- src/main/java/com/study/d09/zoo/Zoo.java
+++ src/main/java/com/study/d09/zoo/Zoo.java
@@ -2,6 +2,15 @@ package com.study.d09.zoo;
 
 public class Zoo {
     public static void main(String[] args) {
-        
+        Animal animal = new Ostrich();
+        showTime(animal);
+    }
+    
+    public static void showTime(Animal animal) {
+        System.out.printf("我是 %s 有 %d 條腿 ", animal.getName(), animal.getLegs());
+        animal.move();
+        if(animal instanceof Ostrich) {
+            ((Ostrich)animal).spirit();
+        }
     }
 }
