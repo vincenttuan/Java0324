a9ceecd14fc8c661b98a31620beb50a2f1d8e1d1
MB-teacher <MB-teacher@192.168.1.109>
2020-04-23 21:36:48 +0800

    Java

--- src/main/java/com/study/d09/pet/PetStore.java
+++ src/main/java/com/study/d09/pet/PetStore.java
@@ -2,14 +2,19 @@ package com.study.d09.pet;
 
 public class PetStore {
     public static void main(String[] args) {
-        Dog d1 = new Chihuahua();
-        Dog d2 = new Shiba();
-        Dog d3 = new PitBull();
+        ADog d1 = new Chihuahua();
+        ADog d2 = new Shiba();
+        ADog d3 = new PitBull();
         Dog d4 = new EDog();
         showTime(d1);
         showTime(d2);
         showTime(d3);
         showTime(d4);
+    }
+    public static void showTime(ADog dog) {
+        System.out.println("活的狗");
+        dog.eat();
+        dog.skill();
     }
     public static void showTime(Dog dog) {
         dog.eat();
