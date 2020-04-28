8af2073d63ba8fc8a5a036f51d7d9cda378621bd
MB-teacher <MB-teacher@192.168.1.109>
2020-04-28 21:22:08 +0800

    Java

--- src/main/java/com/study/d10/LottoMain.java
+++ src/main/java/com/study/d10/LottoMain.java
@@ -2,18 +2,26 @@ package com.study.d10;
 
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class LottoMain {
     public static void main(String[] args) {
         int n = 6;
         Supplier<Integer> s = () -> new Random().nextInt(49) + 1;
         print(n, s);
+        print2(n, s);
     }
     
     public static void print(int n, Supplier<Integer> s) {
         for(int i=1;i<=n;i++) {
-            System.out.print(s.get() + " ");
+            System.out.printf("%d:%d ", i, s.get());
         }
+        System.out.println();
+    }
+    
+    public static void print2(int n, Supplier<Integer> s) {
+        IntStream.rangeClosed(1, n)
+                .forEach((i) -> System.out.printf("%d:%d ", i, s.get()));
         System.out.println();
     }
 }
