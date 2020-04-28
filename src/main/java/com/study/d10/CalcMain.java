39e13b752ab2c2a13538385dcdf3464e7205772e
MB-teacher <MB-teacher@192.168.1.109>
2020-04-28 19:35:54 +0800

    Java

--- src/main/java/com/study/d10/CalcMain.java
+++ src/main/java/com/study/d10/CalcMain.java
@@ -2,6 +2,21 @@ package com.study.d10;
 
 public class CalcMain {
     public static void main(String[] args) {
+        Calc calc = new Calc(){
+            @Override
+            public void action(int x, int y) {
+                System.out.println(x + y);
+            }
+        };
+        calc.action(10, 20);
         
+        Calc calc2 = new Calc(){
+            @Override
+            public void action(int x, int y) {
+                System.out.println(x - y);
+            }
+        };
+        calc2.action(10, 20);
     }
+
 }
