32337bab7ed34b4d75c13b53e0f6926435071fb7
MB-teacher <MB-teacher@192.168.1.109>
2020-04-28 18:58:30 +0800

    Java

---
+++ src/main/java/com/study/d10/CompanyMain.java
@@ -0,0 +1,12 @@
+package com.study.d10;
+
+public class CompanyMain {
+    public static void main(String[] args) {
+        Department dept = new Department();
+        dept.setName("資訊部門");
+        // 該部門員工的工作是
+        Department.Employee emp = dept.new Employee();
+        System.out.print(dept.getName() + "的工作是");
+        emp.job();
+    }
+}
