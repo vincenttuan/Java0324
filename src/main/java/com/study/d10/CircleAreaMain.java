af68d921b662406e3343aa71fb0ac00ddf3e063e
MB-teacher <MB-teacher@192.168.1.109>
2020-04-28 21:11:15 +0800

    Java

--- src/main/java/com/study/d10/CircleAreaMain.java
+++ src/main/java/com/study/d10/CircleAreaMain.java
@@ -5,8 +5,11 @@ import java.util.function.Function;
 public class CircleAreaMain {
     public static void main(String[] args) {
         int [] rs = {10, 9, 8, 7};
-        Function<Integer, Double> f = (r) -> r * r * Math.PI;
+        Function<Integer, Double> f = (r) -> r * r * Math.PI; // 圓面積
+        // (4/3)πr3
+        Function<Integer, Double> f2 = (r) -> (4/3) * Math.PI * Math.pow(r, 3); // 球體積
         print(rs, f);
+        print(rs, f2);
     }
     
     public static void print(int [] rs, Function<Integer, Double> f) {
