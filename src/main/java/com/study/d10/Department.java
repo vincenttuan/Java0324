7996d0aef3933aa7e411919197b18f5af5edb883
MB-teacher <MB-teacher@192.168.1.109>
2020-04-28 19:06:38 +0800

    Java

--- src/main/java/com/study/d10/Department.java
+++ src/main/java/com/study/d10/Department.java
@@ -1,9 +1,12 @@
 package com.study.d10;
 
 // 部門定義
-public class Department { 
+public class Department {
+    static int workTime = 8; // 工作時數
+    int add = 20000; // 部門加給
     // 員工定義
     class Employee {
+        int salary = 40000; // 本薪
         public void job() {
             System.out.println("寫程式");
         }
