c2cc6d1226df6c89a2ca6b047f2ea08ccab92d0d
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 20:44:16 +0800

    Java

--- src/main/java/com/study/d11/collection/Book.java
+++ src/main/java/com/study/d11/collection/Book.java
@@ -1,5 +1,7 @@
 package com.study.d11.collection;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private String author;
@@ -10,6 +12,41 @@ public class Book {
         this.author = author;
         this.price = price;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 17 * hash + Objects.hashCode(this.name);
+        hash = 17 * hash + Objects.hashCode(this.author);
+        hash = 17 * hash + this.price;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Book other = (Book) obj;
+        if (this.price != other.price) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 
     public String getName() {
         return name;
--- src/main/java/com/study/d11/collection/BookDemo.java
+++ src/main/java/com/study/d11/collection/BookDemo.java
@@ -1,10 +1,20 @@
 package com.study.d11.collection;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BookDemo {
     public static void main(String[] args) {
         Book b1 = new Book("Java8", "Vincent", 500);
         Book b2 = new Book("Python先修", "Bob", 400);
         Book b3 = new Book("C++", "Mary", 300);
-        
+        Book b4 = new Book("C++", "Mary", 300);
+        Set<Book> books = new HashSet<>();
+        books.add(b1);
+        books.add(b2);
+        books.add(b3);
+        books.add(b4);
+        System.out.println(books);
+        System.out.println(books.size());
     }
 }
