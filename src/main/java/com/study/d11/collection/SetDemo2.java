3f35f8825c49383c03fc0907bda55b36cf0a5724
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 19:58:37 +0800

    Java

---
+++ src/main/java/com/study/d11/collection/SetDemo2.java
@@ -0,0 +1,22 @@
+package com.study.d11.collection;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class SetDemo2 {
+    public static void main(String[] args) {
+        Set<Integer> score = new HashSet<>(); // 科目
+        score.add(90);   // Integer
+        score.add(100);   // Integer
+        score.add(80);   // Integer
+        System.out.println(score);
+        Iterator<Integer> iter = score.iterator();
+        int sum = 0;
+        while (iter.hasNext()) {
+            Integer next = iter.next();
+            sum += next;
+        }
+        System.out.println(sum);
+    }
+}
