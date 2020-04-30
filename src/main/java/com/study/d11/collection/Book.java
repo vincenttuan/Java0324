bd15eb6ac8c8808ebd7bf52f21953e33f4227ec6
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 20:35:59 +0800

    Java

---
+++ src/main/java/com/study/d11/collection/Book.java
@@ -0,0 +1,44 @@
+package com.study.d11.collection;
+
+public class Book {
+    private String name;
+    private String author;
+    private int price;
+
+    public Book(String name, String author, int price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" + "name=" + name + ", author=" + author + ", price=" + price + '}';
+    }
+    
+    
+}
