8ba58132cef34adf15e88198acb99fd58e328d3d
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 20:37:44 +0800

    Java

--- src/main/java/com/study/d11/collection/BookDemo.java
+++ src/main/java/com/study/d11/collection/BookDemo.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d11.collection;
 
-/**
- *
- * @author MB-teacher
- */
 public class BookDemo {
-    
+    public static void main(String[] args) {
+        Book b1 = new Book("Java8", "Vincent", 500);
+        Book b2 = new Book("Python先修", "Bob", 400);
+        Book b3 = new Book("C++", "Mary", 300);
+        
+    }
 }
