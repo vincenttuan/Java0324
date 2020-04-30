b574d3f8e74a85585fc3d741513c758279b4e750
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 19:15:56 +0800

    Java

--- src/main/java/com/study/d11/RedEnvelopeDemo.java
+++ src/main/java/com/study/d11/RedEnvelopeDemo.java
@@ -1,21 +1,25 @@
 package com.study.d11;
 
+import java.util.stream.Stream;
+
 public class RedEnvelopeDemo {
     public static void main(String[] args) {
         RedEnvelope re = RedEnvelope.雙親;
         System.out.println(re.getMoney());
-        
         RedEnvelope[] res = {
             RedEnvelope.雙親,
             RedEnvelope.老婆,
             RedEnvelope.小孩    
         };
-        
+        // Java 7 以前的寫法
         int sum = 0;
         for(RedEnvelope r : res) {
             sum += r.getMoney();
         }
         System.out.println(sum);
+        // Java 8
+        int sum2 = Stream.of(res).mapToInt(r -> r.getMoney()).sum();
+        System.out.println(sum2);
         
     }
 }
