ab6f689f0871103df9e694b8988b8b52b124e656
MB-teacher <MB-teacher@192.168.1.109>
2020-04-30 18:58:03 +0800

    Java

---
+++ src/main/java/com/study/d11/USCurrency.java
@@ -0,0 +1,18 @@
+package com.study.d11;
+
+public enum USCurrency {
+    PENNY(1),    // 一美分
+    NICKLE(5),   // 五美分
+    DIME(10),    // 十美分
+    QUARTER(25); // 25美分
+    
+    private int value;
+ 
+    private USCurrency(int value) {
+        this.value = value;
+    }
+ 
+    public int getValue() {
+        return value;
+    }
+}
