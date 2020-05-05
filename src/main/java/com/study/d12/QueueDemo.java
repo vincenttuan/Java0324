f5f7d9e3d1f6df0c367fba05039459f90a90526b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-05 20:45:25 +0800

    Java

---
+++ src/main/java/com/study/d12/QueueDemo.java
@@ -0,0 +1,22 @@
+package com.study.d12;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> q = new LinkedList<>();
+        q.offer("吃東西");
+        q.offer("吞下去");
+        q.offer("消化他");
+        q.offer("拉出來");
+        System.out.println(q);
+        
+        while (q.size() > 0) {            
+            String data = q.poll();
+            System.out.println("現在進行: " + data);
+        }
+        System.out.println(q);
+        
+    }
+}
