4a494ab025adc451dcb7b372243b35b01db63ca4
MB-teacher <MB-teacher@192.168.1.109>
2020-05-05 20:59:05 +0800

    Java

--- src/main/java/com/study/d12/VectorDemo.java
+++ src/main/java/com/study/d12/VectorDemo.java
@@ -4,10 +4,11 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector<Integer> v = new Vector<>();
+        Vector<Integer> v = new Vector<>(41, 5);
         for(int i=1;i<=41;i++) {
             v.add(i);
         }
+        v.add(42);
         System.out.println("size: " + v.size()); // 有使用的元素個數
         System.out.println("capacity: " + v.capacity()); // 空間數量
         
