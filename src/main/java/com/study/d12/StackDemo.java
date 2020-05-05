2b51d4a28e01cb2f980031bd9956b10ee090f621
MB-teacher <MB-teacher@192.168.1.109>
2020-05-05 21:07:41 +0800

    Java

---
+++ src/main/java/com/study/d12/StackDemo.java
@@ -0,0 +1,15 @@
+package com.study.d12;
+
+import java.util.Scanner;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請輸入任意字串:");
+        String str = sc.nextLine();
+        // 請利用 Stack 做出字串反轉效果 Ex: Java -> avaJ
+        char[] chars = str.toCharArray(); // Java -> {'J', 'a', 'v', 'a'}
+        
+        
+    }
+}
