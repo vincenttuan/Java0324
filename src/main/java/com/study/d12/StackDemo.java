f3d469e8d7612d8d449eef33ae6e20d523ff3852
MB-teacher <MB-teacher@192.168.1.109>
2020-05-05 21:13:43 +0800

    Java

--- src/main/java/com/study/d12/StackDemo.java
+++ src/main/java/com/study/d12/StackDemo.java
@@ -1,6 +1,7 @@
 package com.study.d12;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class StackDemo {
     public static void main(String[] args) {
@@ -9,7 +10,14 @@ public class StackDemo {
         String str = sc.nextLine();
         // 請利用 Stack 做出字串反轉效果 Ex: Java -> avaJ
         char[] chars = str.toCharArray(); // Java -> {'J', 'a', 'v', 'a'}
-        
+        Stack<Character> stack = new Stack<>();
+        for(char c : chars) {
+            stack.push(c);
+        }
+        while (!stack.isEmpty()) {            
+            char data = stack.pop();
+            System.out.print(data);
+        }
         
     }
 }
