e5da8bebbd81da5898b25d25cd193997faf6ba41
MB-teacher <MB-teacher@192.168.1.109>
2020-05-07 18:48:52 +0800

    Java

---
+++ src/main/java/com/study/d13/HashMapDemo.java
@@ -0,0 +1,14 @@
+package com.study.d13;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("John", 40000);
+        map.put("Mary", 50000);
+        map.put("Tom", 60000);
+        System.out.println(map);
+    }
+}
