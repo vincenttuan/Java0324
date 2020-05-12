730ffae1bbea3eeffd7502393843c4afd0e448f7
MB-teacher <MB-teacher@192.168.1.109>
2020-05-12 21:42:31 +0800

    Java

--- src/main/java/com/study/d14/MaskDemo.java
+++ src/main/java/com/study/d14/MaskDemo.java
@@ -1,5 +1,9 @@
 package com.study.d14;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -9,6 +13,15 @@ public class MaskDemo {
         URL url = new URL(path);
         Scanner sc = new Scanner(url.openStream(), "UTF-8").useDelimiter("\\A");
         String json = sc.next();
-        System.out.println(json);
+        JsonElement je = JsonParser.parseString(json); // 整體 Json 元素
+        JsonObject root = je.getAsJsonObject(); // Json 根物件
+        JsonArray array = root.getAsJsonArray("features");
+        // Java 8
+        array.forEach(jo -> {
+            jo = jo.getAsJsonObject().get("properties").getAsJsonObject();
+            if(jo.getAsJsonObject().get("mask_adult").getAsInt() > 4000) {
+                System.out.println(jo);
+            }
+        });
     }
 }
