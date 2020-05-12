1a6de60cbdd0c779518be30945e32dd9d3c097ba
MB-teacher <MB-teacher@192.168.1.109>
2020-05-12 19:47:27 +0800

    Java

---
+++ src/main/java/com/study/d14/MyStock.java
@@ -0,0 +1,11 @@
+package com.study.d14;
+
+public class MyStock {
+    public String 證券代號;
+    public String 證券名稱;
+    public String 殖利率;
+    public String 股利年度;
+    public String 本益比;
+    public String 股價淨值比;
+    public String 財報年季;
+}
