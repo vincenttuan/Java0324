eff3530b602e77f15e3ae9a97b5854dfaa749dd1
MB-teacher <MB-teacher@192.168.1.109>
2020-05-12 21:28:13 +0800

    Java

---
+++ src/main/java/com/study/d14/MaskDemo.java
@@ -0,0 +1,14 @@
+package com.study.d14;
+
+import java.net.URL;
+import java.util.Scanner;
+
+public class MaskDemo {
+    public static void main(String[] args) throws Exception {
+        String path = "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+        URL url = new URL(path);
+        Scanner sc = new Scanner(url.openStream()).useDelimiter("\\A");
+        String json = sc.next();
+        System.out.println(json);
+    }
+}
