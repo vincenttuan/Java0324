60c33e7e004ac5fc038751406f311964ac225044
MB-teacher <MB-teacher@192.168.1.109>
2020-05-12 20:41:11 +0800

    Java

--- src/main/java/com/study/d14/MyStockDemo.java
+++ src/main/java/com/study/d14/MyStockDemo.java
@@ -1,6 +1,8 @@
 package com.study.d14;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MyStockDemo {
@@ -10,12 +12,15 @@ public class MyStockDemo {
         String data = sc.next();
         //System.out.println(data);
         String[] rows = data.split("\n");
+        List<MyStock> myStocks = new ArrayList<>();
         for(String row : rows) {
             String[] stock = row.trim().replace("\"", "").split(",");
             if(stock.length == 7 && !stock[0].contains("證券代號")) {
-                System.out.println(stock[0] + " " + stock[1] + " " + stock[2] + " " + stock[3] + " " + stock[4] + " " + stock[5] + " " + stock[6]);
-                
+                //System.out.println(stock[0] + " " + stock[1] + " " + stock[2] + " " + stock[3] + " " + stock[4] + " " + stock[5] + " " + stock[6]);
+                MyStock myStock = new MyStock(stock[0], stock[1], stock[2], stock[3], stock[4], stock[5], stock[6]);
+                myStocks.add(myStock);
             }
         }
+        System.out.println(myStocks);
     }
 }
