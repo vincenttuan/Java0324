7446b82301a3d993d67c741de70651dfb627b243
MB-teacher <MB-teacher@192.168.1.109>
2020-05-14 20:34:47 +0800

    Java

--- src/main/java/com/study/d15/BMIDemo2.java
+++ src/main/java/com/study/d15/BMIDemo2.java
@@ -1,7 +1,9 @@
 package com.study.d15;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -11,5 +13,26 @@ public class BMIDemo2 {
         Scanner sc = new Scanner(file, "UTF-8").useDelimiter("\\A");
         String data = sc.next();
         System.out.println(data);
+        String[] rows = data.split("\n");
+        
+        List<BMI> list = new ArrayList<>();
+        for(String row : rows) {
+            // 小明,170,60
+            String[] rowArray = row.split(",");
+            String name = rowArray[0].trim();
+            double height = Double.parseDouble(rowArray[1].trim());
+            double weight = Double.parseDouble(rowArray[2].trim());
+            BMI bmi = new BMI(name, height, weight);
+            list.add(bmi);
+        }
+        System.out.println(list);
+        
+        list.stream()
+                .sorted(Comparator.comparing(BMI::getName))
+                .forEach(System.out::println);
+        
+        list.stream()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
