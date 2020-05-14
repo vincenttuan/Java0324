0aebce5557ddff49433babd4c5565ced2cf0734b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-14 19:09:49 +0800

    Java

--- src/main/java/com/study/d15/PersonDemo.java
+++ src/main/java/com/study/d15/PersonDemo.java
@@ -5,15 +5,26 @@ import java.util.Comparator;
 import java.util.stream.Stream;
 
 public class PersonDemo {
+
     public static void main(String[] args) {
         Person p1 = new Person("John", 30);
         Person p2 = new Person("Mary", 20);
         Person p3 = new Person("Helen", 40);
-        
+
         Stream.of(p1, p2, p3).sorted().forEach(System.out::println);
-        
+
         Stream.of(p1, p2, p3)
                 .sorted(Comparator.comparing(Person::getAge).reversed())
                 .forEach(System.out::println);
+
+        Stream.of(p1, p2, p3)
+                .sorted(new Comparator<Person>() {
+                    @Override
+                    public int compare(Person o1, Person o2) {
+                        return o1.getAge() - o2.getAge();
+                    }
+                })
+                .forEach(System.out::println);
+
     }
 }
