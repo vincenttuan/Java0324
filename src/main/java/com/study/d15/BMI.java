92495465417ec889c6b6ec383ca51992ade43bf8
MB-teacher <MB-teacher@192.168.1.109>
2020-05-14 19:40:07 +0800

    Java

---
+++ src/main/java/com/study/d15/BMI.java
@@ -0,0 +1,48 @@
+package com.study.d15;
+
+public class BMI implements Comparable<BMI>{
+    private String name;
+    private double height;
+    private double weight;
+
+    public BMI(String name, double height, double weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "BMI{" + "name=" + name + ", height=" + height + ", weight=" + weight + '}';
+    }
+
+    @Override
+    public int compareTo(BMI o) {
+        return name.compareTo(o.name);
+    }
+    
+}
