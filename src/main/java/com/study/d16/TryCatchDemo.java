7ded6ef137261b4ab9909aa7ee6b2f3aa00cfac2
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 18:50:05 +0800

    Java

---
+++ src/main/java/com/study/d16/TryCatchDemo.java
@@ -0,0 +1,10 @@
+package com.study.d16;
+
+import java.io.File;
+
+public class TryCatchDemo {
+    public static void main(String[] args) {
+        File file = new File("src\\main\\java\\com\\study\\d16\\numbers.txt");
+        
+    }
+}
