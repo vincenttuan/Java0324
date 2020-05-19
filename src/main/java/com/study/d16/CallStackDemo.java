d140082169812dbdfbc616ebfaf8c88f95105101
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 20:00:00 +0800

    Java

--- src/main/java/com/study/d16/CallStackDemo.java
+++ src/main/java/com/study/d16/CallStackDemo.java
@@ -10,7 +10,9 @@ public class CallStackDemo {
         try {
             bar(y);
         } catch (Exception e) {
-            System.out.println("foo() 處理了, " + e.getMessage());
+            System.out.println("錯誤訊息: " + e.getMessage());
+            System.out.println("錯誤字串: " + e);
+            e.printStackTrace(System.out); // 在 System.out 上印出完整錯誤(錯誤在堆疊中的內容)
         }
     }
     
