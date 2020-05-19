a17fdda09e5551ed5785a3a883d7a63e1aa814d7
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 19:29:55 +0800

    Java

---
+++ src/main/java/com/study/d16/TryCatchDemo2.java
@@ -0,0 +1,20 @@
+package com.study.d16;
+
+public class TryCatchDemo2 {
+    public static void main(String[] args) {
+        int x = 500; // 分子
+        Integer[] ys = {10, 20, 0, 40, null, 60}; // 分母
+        // 請求 x / y 結果
+        for(Integer y : ys) {
+            // 可能的錯誤是: ArithmeticException, NullPointerException
+            try {
+                System.out.printf("%d / %d = %d\n", x, y, x/y);
+            } catch (ArithmeticException | NullPointerException e) {
+                // 不處理
+            }
+            
+        }
+        
+        
+    }
+}
