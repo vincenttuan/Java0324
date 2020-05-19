b1dafd4de5bf830dba5916f2518600186207a5e3
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 19:56:17 +0800

    Java

---
+++ src/main/java/com/study/d16/CallStackDemo.java
@@ -0,0 +1,25 @@
+package com.study.d16;
+
+public class CallStackDemo {
+    public static void main(String[] args) {
+        int y = 0;
+        try {
+            foo(y);
+        } catch (Exception e) {
+            System.out.println("main() 處理了, " + e.getMessage());
+        }
+    }
+    
+    public static void foo(int y) {
+        try {
+            bar(y);
+        } catch (Exception e) {
+            System.out.println("foo() 處理了, " + e.getMessage());
+        }
+    }
+    
+    public static void bar(int y) {
+        System.out.println(500/y);
+    }
+    
+}
