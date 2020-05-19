dcf65d0115a03e236d2e14498496d167aefd04f6
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 21:26:21 +0800

    Java

--- src/main/java/com/study/d16/MemberDemo.java
+++ src/main/java/com/study/d16/MemberDemo.java
@@ -6,13 +6,16 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.logging.Level;
 
 public class MemberDemo {
     public static void main(String[] args) {
         try {
-            boolean loginCheck = login("john", "1234");
+            boolean loginCheck = login("jo", "1111");
             System.out.println("loginCheck: " + loginCheck);
         } catch (FileNotFoundException ex) {
             System.out.println("檔案找不到");
@@ -24,6 +27,7 @@ public class MemberDemo {
     public static boolean login(String username, String password) throws FileNotFoundException, Exception {
         isMember(username);
         isActive(username);
+        isExpired(username);
         checkUsernameAndPassowrd(username, password);
         return true;
     }
@@ -56,6 +60,27 @@ public class MemberDemo {
             }
         }
         return true;
+    }
+    
+    public static boolean isExpired(String username) throws FileNotFoundException, Exception {
+        File file = new File("src\\main\\java\\com\\study\\d16\\member.json");
+        Scanner sc = new Scanner(file).useDelimiter("\\A");
+        String json = sc.next();
+        JsonArray ja = JsonParser.parseString(json).getAsJsonArray();
+        for(int i=0;i<ja.size();i++) {
+            JsonObject jo = ja.get(i).getAsJsonObject();
+            if(jo.get("username").getAsString().equals(username)) {
+                String expired = jo.get("expired").getAsString();
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date expiredDate = sdf.parse(expired);
+                Date today = new Date();
+                if(expiredDate.getTime() < today.getTime()) {
+                    Exception e = new Exception(username + " 會員資格已於 " + sdf.format(expiredDate) + " 到期");
+                    throw e;
+                }
+            }
+        }
+        return false;
     }
     
     public static boolean checkUsernameAndPassowrd(String username, String password) throws FileNotFoundException, Exception {
