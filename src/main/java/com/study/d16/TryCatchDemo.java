6b9a0fa8086dd4d2aea522967cf8a53889de8ad8
MB-teacher <MB-teacher@192.168.1.109>
2020-05-19 19:11:29 +0800

    Java

--- src/main/java/com/study/d16/TryCatchDemo.java
+++ src/main/java/com/study/d16/TryCatchDemo.java
@@ -15,12 +15,10 @@ public class TryCatchDemo {
             int x = 500; // 分子
             String[] array = data.split("\n");
             for(String arr : array) {
-                try {
-                    System.out.printf("%d / %d = %d\n", x, Integer.parseInt(arr), (x / Integer.parseInt(arr)) );
-                } catch (ArithmeticException e) {
-                    System.out.println("數學錯誤, 原因:" + e.getMessage());
+                int y = Integer.parseInt(arr); // 分母
+                if(y != 0) {
+                    System.out.printf("%d / %d = %d\n", x, y, (x / y) );
                 }
-                
             }
         } catch (FileNotFoundException ex) {
             System.out.println("檔案有問題, 原因:" + ex.getMessage());
