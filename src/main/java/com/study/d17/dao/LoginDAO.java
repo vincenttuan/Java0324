2fffd44eda48bdb7f6135654994cbccde438f37c
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 19:18:38 +0800

    Java

--- src/main/java/com/study/d17/dao/LoginDAO.java
+++ src/main/java/com/study/d17/dao/LoginDAO.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.dao;
 
-/**
- *
- * @author MB-teacher
- */
+import com.google.gson.Gson;
+import com.study.d17.entity.Member;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class LoginDAO {
-    
+    private static Member[] members;
+    static {
+        try {
+            File file = new File("src\\main\\java\\com\\study\\d17\\db\\member.json");
+            String json = new Scanner(file).useDelimiter("\\A").next();
+            members = new Gson().fromJson(json, Member[].class);
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        }
+    }
+    public Member[] getMembers() {
+        return members;
+    }
 }
