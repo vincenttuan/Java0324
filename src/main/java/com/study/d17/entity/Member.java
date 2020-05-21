3380ba820a7d90d75398106a0db48b69e561e8cd
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 19:10:52 +0800

    Java

--- src/main/java/com/study/d17/entity/Member.java
+++ src/main/java/com/study/d17/entity/Member.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.entity;
 
-/**
- *
- * @author MB-teacher
- */
+import java.util.Date;
+
 public class Member {
+    private String username;
+    private String password;
+    private int salary;
+    private boolean active;
+    private Date expired;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getExpired() {
+        return expired;
+    }
+
+    public void setExpired(Date expired) {
+        this.expired = expired;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" + "username=" + username + ", password=" + password + ", salary=" + salary + ", active=" + active + ", expired=" + expired + '}';
+    }
     
 }
