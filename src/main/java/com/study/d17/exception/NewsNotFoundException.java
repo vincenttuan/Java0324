183c026243aabc22116ecd3ab95288901c3dc86d
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 21:07:45 +0800

    Java

--- src/main/java/com/study/d17/exception/NewsNotFoundException.java
+++ src/main/java/com/study/d17/exception/NewsNotFoundException.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.exception;
 
-/**
- *
- * @author MB-teacher
- */
-public class NewsNotFoundException {
+public class NewsNotFoundException extends Exception {
+
+    public NewsNotFoundException(String string) {
+        super(string);
+    }
     
+    public void why() {
+        System.out.println("News 只有最近 30 天的資料 !");
+    }
 }
