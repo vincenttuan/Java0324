b96cfcbc1c380d8289059c40876dcc49f3b12702
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 19:24:54 +0800

    Java

--- src/main/java/com/study/d17/exception/LoginException.java
+++ src/main/java/com/study/d17/exception/LoginException.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.exception;
 
-/**
- *
- * @author MB-teacher
- */
-public class LoginException {
+public class LoginException extends Exception {
+
+    public LoginException(String string) {
+        super(string);
+    }
     
 }
--- src/main/java/com/study/d17/service/LoginService.java
+++ src/main/java/com/study/d17/service/LoginService.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.service;
 
-/**
- *
- * @author MB-teacher
- */
+import com.study.d17.dao.LoginDAO;
+import com.study.d17.entity.Member;
+import com.study.d17.exception.LoginException;
+
 public class LoginService {
-    
+    private LoginDAO dao = new LoginDAO();
+    public boolean login(String username, String password) throws LoginException {
+        Member[] members = dao.getMembers();
+        for(Member m : members) {
+            if(m.getUsername().equals(username) && m.getPassword().equals(password)) {
+                return true;
+            }
+        }
+        LoginException ex = new LoginException("登入失敗");
+        throw ex;
+    }
 }
