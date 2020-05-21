46f4e6631b89417396cf0d57238e6eb3952a0544
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 21:10:24 +0800

    Java

--- src/main/java/com/study/d17/service/NewsService.java
+++ src/main/java/com/study/d17/service/NewsService.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.study.d17.service;
 
-/**
- *
- * @author MB-teacher
- */
+import com.study.d17.dao.NewsDAO;
+import com.study.d17.entity.Data;
+
 public class NewsService {
+    private NewsDAO dao = new NewsDAO();
     
+    public Data getNews(String date) {
+        return dao.getDataByDate(date);
+    }
 }
