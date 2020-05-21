bebb529c762838596dff3b56f7056e2a7c7cc931
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 21:30:19 +0800

    Java

--- src/main/java/com/study/d17/NewsPage.java
+++ src/main/java/com/study/d17/NewsPage.java
@@ -1,21 +1,24 @@
 package com.study.d17;
 
 import com.study.d17.entity.Data;
+import com.study.d17.exception.NewsNotFoundException;
 import com.study.d17.service.NewsService;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class NewsPage {
     public static void main(String[] args) {
         NewsService service = new NewsService();
-        Data data = service.getNews("5/22");
-        System.out.println(data.getTitle());
-        /*
-        for(List<String> list : data.getHeadlines()) {
-            list.stream().forEach(System.out::println);
+        try {
+            Data data = service.getNews("5/22");
+            System.out.println(data.getTitle());
+            data.getHeadlines()
+                    .stream()
+                    .forEach(list -> list.stream().forEach(System.out::println));
+        } catch (NewsNotFoundException ex) {
+            System.out.println(ex);
+            ex.why();
         }
-        */
-        data.getHeadlines()
-                .stream()
-                .forEach(list -> list.stream().forEach(System.out::println));
     }
 }
--- src/main/java/com/study/d17/service/NewsService.java
+++ src/main/java/com/study/d17/service/NewsService.java
@@ -2,11 +2,12 @@ package com.study.d17.service;
 
 import com.study.d17.dao.NewsDAO;
 import com.study.d17.entity.Data;
+import com.study.d17.exception.NewsNotFoundException;
 
 public class NewsService {
     private NewsDAO dao = new NewsDAO();
     
-    public Data getNews(String date) {
+    public Data getNews(String date) throws NewsNotFoundException {
         return dao.getDataByDate(date);
     }
 }
