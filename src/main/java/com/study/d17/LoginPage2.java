2294a6b0bf9d8262f98d4b25f7b623d7fdb4cca2
MB-teacher <MB-teacher@192.168.1.109>
2020-05-21 19:47:19 +0800

    Java

---
+++ src/main/java/com/study/d17/LoginPage2.java
@@ -0,0 +1,26 @@
+package com.study.d17;
+
+import com.study.d17.entity.Member;
+import com.study.d17.exception.LoginException;
+import com.study.d17.service.LoginService;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginPage2 {
+    public static void main(String[] args) {
+        LoginService service = new LoginService();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("請輸入 username : ");
+        String username = sc.next();
+        System.out.print("請輸入 password : ");
+        String password = sc.next();
+        try {
+            Member member = service.loginAndMember(username, password);
+            System.out.println(member);
+        } catch (LoginException ex) {
+            System.out.println(ex);
+        }
+        
+    }
+}
