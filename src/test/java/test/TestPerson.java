32f389886d8f54ebb9603838b802dd86ad1a80f7
MB-teacher <MB-teacher@192.168.1.109>
2020-05-26 20:44:15 +0800

    Java

--- src/test/java/test/TestPerson.java
+++ src/test/java/test/TestPerson.java
@@ -18,4 +18,24 @@ public class TestPerson {
         assert (excepted == act) : excepted + " == " + act;
         System.out.println("Pass");
     }
+    
+    @Test
+    public void test2() {
+        //Arrange
+        Person p = new Person();
+        int age = 120;
+        int excepted = -1;
+        //Act
+        p.setAge(age);
+        int act = p.getAge();
+        //Assert
+        assert (excepted == act) : excepted + " == " + act;
+        System.out.println("Pass");
+    }
+    
+    @Test
+    public void testAll() {
+        test();
+        test2();
+    }
 }
