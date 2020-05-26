99745dccc5a584b6b4ef81f795ded1d527d5c189
MB-teacher <MB-teacher@192.168.1.109>
2020-05-26 20:42:40 +0800

    Java

--- src/main/java/com/study/d18/Person.java
+++ src/main/java/com/study/d18/Person.java
@@ -8,9 +8,10 @@ public class Person {
     }
 
     public void setAge(int age) {
-        // ...
-        //age++;
-        assert age >= 18 && age <= 150 : age + "歲不合法";
-        this.age = age;
+        if (age >= 18 && age < 120) {
+            this.age = age;
+        } else {
+            this.age = -1;
+        }
     }
 }
