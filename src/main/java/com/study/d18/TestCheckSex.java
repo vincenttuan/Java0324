382b2e759c99056c2b7c9ca979e445a3e74ecff1
MB-teacher <MB-teacher@192.168.1.109>
2020-05-26 19:27:50 +0800

    Java

--- src/main/java/com/study/d18/TestCheckSex.java
+++ src/main/java/com/study/d18/TestCheckSex.java
@@ -2,6 +2,14 @@ package com.study.d18;
 
 public class TestCheckSex {
     public static void main(String[] args) {
-        
+        // Arrange
+        CheckSex cs = new CheckSex();
+        String id = "A1223344556";
+        String excepted = "男生";
+        // Act
+        String act = cs.getSexByID(id);
+        // Assert
+        assert (excepted.equals(act));
+        System.out.println("Pass");
     }
 }
