d14d558e546cf9dbcaa9fdab1293e33aa40cdfc6
MB-teacher <MB-teacher@192.168.1.109>
2020-05-26 20:11:51 +0800

    Java

---
+++ src/test/java/test/TestCalc.java
@@ -0,0 +1,23 @@
+package test;
+
+import com.study.d18.Calc;
+import org.junit.Test;
+
+public class TestCalc {
+    @Test
+    public void test() {
+        // 3A Arrange Act Assert
+        // Arrange
+        Calc calc = new Calc();
+        int x = 5;
+        int y = 10;
+        int excepted = 15; // 期望
+        // Act
+        int act = calc.add(x, y);
+        // Assert
+        assert(excepted == act): excepted + " == " + act;
+        
+        System.out.println("Pass");
+        
+    }
+}
