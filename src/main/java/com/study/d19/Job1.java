d7839b382b5ad6f5e83ea99125210165c7a8576b
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 19:19:03 +0800

    Java

--- src/main/java/com/study/d19/Job1.java
+++ src/main/java/com/study/d19/Job1.java
@@ -1,6 +1,12 @@
 package com.study.d19;
 
-public class Job1 {
+public class Job1 extends Thread {
+    
+    @Override
+    public void run() {
+        work();
+    }
+    
     public void work() {
         String threadName = Thread.currentThread().getName(); // 執行緒名字
         for(int i=1;i<=1000;i++) {
---
+++ src/main/java/com/study/d19/MultiThread.java
@@ -0,0 +1,7 @@
+package com.study.d19;
+
+public class MultiThread {
+    public static void main(String[] args) {
+        
+    }
+}
