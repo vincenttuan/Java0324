5ba3683e2254d737c7fde8c69644d7c32fefd093
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 21:24:41 +0800

    Java

---
+++ src/main/java/com/study/d19/lab/RaceDemo2.java
@@ -0,0 +1,24 @@
+package com.study.d19.lab;
+
+import java.util.Random;
+
+public class RaceDemo2 {
+    private static String animal = "鼠牛虎兔龍蛇馬羊猴雞狗豬";
+    public static void main(String[] args) {
+        Runnable r = () -> {
+            int step = 10_0000_000;
+            for(int i=1;true;i++) {
+                if(i == step) {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.printf("%s 到終點了\n", threadName);
+                    break;
+                }
+            }
+        };
+        for(char a : animal.toCharArray()) {
+            Thread t = new Thread(r, String.valueOf(a));
+            t.start();
+        }
+        
+    }
+}
