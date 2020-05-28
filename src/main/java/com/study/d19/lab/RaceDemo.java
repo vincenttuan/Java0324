73607d88aa9c6990091e443b71cd0a6ce2ab108c
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 21:13:11 +0800

    Java

--- src/main/java/com/study/d19/lab/RaceDemo.java
+++ src/main/java/com/study/d19/lab/RaceDemo.java
@@ -1,13 +1,15 @@
 package com.study.d19.lab;
 
 public class RaceDemo {
+    private static boolean gameover = false;
     public static void main(String[] args) {
         Runnable r = () -> {
             int step = 1000;
-            for(int i=1;true;i++) {
+            for(int i=1;true && !gameover;i++) {
                 String threadName = Thread.currentThread().getName();
                 System.out.printf("%s 跑了 %d 步\n", threadName, i);
                 if(i == step) {
+                    gameover = true;
                     System.out.printf("%s 到終點了\n", threadName);
                     break;
                 }
