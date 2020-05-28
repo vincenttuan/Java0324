e50c318ed508528d7cef495431ae2b861e799464
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 19:11:00 +0800

    Java

--- src/main/java/com/study/d19/Job1.java
+++ src/main/java/com/study/d19/Job1.java
@@ -2,8 +2,9 @@ package com.study.d19;
 
 public class Job1 {
     public void work() {
+        String threadName = Thread.currentThread().getName(); // 執行緒名字
         for(int i=1;i<=1000;i++) {
-            System.out.printf("搬第 %d 本書\n", i);
+            System.out.printf("%s 搬第 %d 本書\n", threadName, i);
         }
     }
 }
--- src/main/java/com/study/d19/Job2.java
+++ src/main/java/com/study/d19/Job2.java
@@ -2,8 +2,9 @@ package com.study.d19;
 
 public class Job2 {
     public void work() {
+        String threadName = Thread.currentThread().getName(); // 執行緒名字
         for(int i=1;i<=1000;i++) {
-            System.out.printf("發第 %d 張 DM\n", i);
+            System.out.printf("%s 發第 %d 張 DM\n", threadName, i);
         }
     }
 }
