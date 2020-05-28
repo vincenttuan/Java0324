ff0c8306592e6b99c27ce90a1edbd2120b8178e1
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 19:54:38 +0800

    Java

---
+++ src/main/java/com/study/d19/runnable/MultiThread2.java
@@ -0,0 +1,24 @@
+package com.study.d19.runnable;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class MultiThread2 { 
+    public static void main(String[] args) {
+        // Java 8
+        Runnable r1 = () -> System.out.println(new Random().nextInt(100));
+        Runnable r2 = () -> {
+            Set<Integer> lotto = new TreeSet<>();
+            while (lotto.size() < 5) {                
+                lotto.add(new Random().nextInt(39) + 1);
+            }
+            System.out.println(lotto);
+        };
+        Thread t1 = new Thread(r1);
+        t1.start();
+        Thread t2 = new Thread(r2);
+        t2.start();
+        
+    }
+}
