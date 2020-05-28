f340d00ec007aac317aa0102c183aa5b754d2190
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 19:12:22 +0800

    Java

--- src/main/java/com/study/d19/SingleThread.java
+++ src/main/java/com/study/d19/SingleThread.java
@@ -2,6 +2,9 @@ package com.study.d19;
 
 public class SingleThread {
     public static void main(String[] args) {
-        
+        Job1 job1 = new Job1();
+        Job2 job2 = new Job2();
+        job1.work();
+        job2.work();
     }
 }
