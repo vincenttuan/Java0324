1cb70593257295d5de69f2a69b1a6209ae09bd35
MB-teacher <MB-teacher@192.168.1.109>
2020-05-28 19:33:03 +0800

    Java

--- src/main/java/com/study/d19/MultiThread2.java
+++ src/main/java/com/study/d19/MultiThread2.java
@@ -1,11 +1,12 @@
 package com.study.d19;
 
-public class MultiThread {
+public class MultiThread2 {
     public static void main(String[] args) {
         Job1 job1 = new Job1(); // user thread
-        Job2 job2 = new Job2(); // user thread
+        Job2 job2 = new Job2(); // daemon thread
         job1.setName("John");
         job2.setName("Mary");
+        job2.setDaemon(true);
         job1.start();
         job2.start();
     }
