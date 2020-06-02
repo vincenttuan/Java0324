37985d82bfebc04479eda0c309e7dac86d3c5d85
MB-teacher <MB-teacher@192.168.1.109>
2020-06-02 18:51:45 +0800

    Java

---
+++ src/main/java/com/study/d20/CallableDemo.java
@@ -0,0 +1,23 @@
+package com.study.d20;
+
+import java.util.Random;
+
+class GetLuckyNumber extends Thread {
+    @Override
+    public void run() {
+        System.out.println("資料計算中...");
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+        }
+        int number = new Random().nextInt(100);
+        System.out.printf("Lucky number is %d\n", number);
+    }
+}
+
+public class CallableDemo {
+    public static void main(String[] args) {
+        GetLuckyNumber num = new GetLuckyNumber();
+        num.start();
+    }
+}
