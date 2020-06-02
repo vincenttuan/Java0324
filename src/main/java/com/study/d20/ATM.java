8dbd3860a076fab765ef5d52d5e861b9ee1ce7a2
MB-teacher <MB-teacher@192.168.1.109>
2020-06-02 21:08:49 +0800

    Java

--- src/main/java/com/study/d20/ATM.java
+++ src/main/java/com/study/d20/ATM.java
@@ -18,7 +18,20 @@ class Account {
     
     // 提款
     public void withdraw(int money) {
-        
+        String who = Thread.currentThread().getName();
+        System.out.printf("%s 開始提款\n", who);
+        int current_balance = getBalance(); // 取得目前的帳戶餘額
+        System.out.printf("%s 看到目前的帳戶餘額是: $%d\n", who, current_balance);
+        // 進行提款之行為
+        if(current_balance >= money) {
+            // 帳戶餘額扣款
+            setBalance(current_balance - money);
+            // 提款成功
+            System.out.printf("%s 提款 $%d 成功, 帳戶餘額 $%d\n", who, money, getBalance());
+        } else {
+            // 提款失敗
+            System.out.printf("%s 提款 $%d 失敗, 帳戶餘額 $%d\n", who, money, getBalance());
+        }
     }
 }
 
