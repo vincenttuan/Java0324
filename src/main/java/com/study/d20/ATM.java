1f47cdded33c1ab769767cae1fcf9bfa19332a00
MB-teacher <MB-teacher@192.168.1.109>
2020-06-02 21:15:34 +0800

    Java

--- src/main/java/com/study/d20/ATM.java
+++ src/main/java/com/study/d20/ATM.java
@@ -55,7 +55,13 @@ public class ATM {
     public static void main(String[] args) {
         Account account = new Account(10000); // 帳號有 $10000 元
         Withdraw w1 = new Withdraw(account, 5000); // 欲提款 $5000 元的工作
+        Withdraw w2 = new Withdraw(account, 4000); // 欲提款 $4000 元的工作
+        Withdraw w3 = new Withdraw(account, 3000); // 欲提款 $3000 元的工作
         Thread t1 = new Thread(w1, "小明"); // 指派小明做 w1 的工作
+        Thread t2 = new Thread(w2, "小華"); // 指派小華做 w2 的工作
+        Thread t3 = new Thread(w3, "小英"); // 指派小英做 w3 的工作
         t1.start(); // 小明開始工作
+        t2.start(); // 小華開始工作
+        t3.start(); // 小英開始工作
     }
 }
