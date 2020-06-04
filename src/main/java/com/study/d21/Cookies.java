c39b3019515b8596f2986b1bac3a9f19b19f0011
MB-teacher <MB-teacher@192.168.1.109>
2020-06-04 19:11:07 +0800

    Java

--- src/main/java/com/study/d21/Cookies.java
+++ src/main/java/com/study/d21/Cookies.java
@@ -1,12 +1,29 @@
 package com.study.d21;
 
 public class Cookies {
+    private boolean empty = true; // 預設盤子是空的
     
-    public void put(int no) { // 放餅乾
+    public synchronized void put(int no) { // 放餅乾
+        if(!empty) {
+            try {
+                wait();
+            } catch (Exception e) {
+            }
+        }
         System.out.printf("主人放了第 %d 塊餅乾\n", no);
+        empty = false; // 盤子上已有餅乾
+        notify(); // 喚醒正在 wait 的執行緒
     }
     
-    public void eat(int no) { // 吃餅乾
+    public synchronized void eat(int no) { // 吃餅乾
+        if(empty) {
+            try {
+                wait(); // 等待...
+            } catch (Exception e) {
+            }
+        }
         System.out.printf("小狗吃了第 %d 塊餅乾\n", no);
+        empty = true; // 盤子已空
+        notify(); // 喚醒正在 wait 的執行緒
     }
 }
