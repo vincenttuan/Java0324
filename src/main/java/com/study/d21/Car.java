60d4cfb4c1e62d3b55924ab2688aaa050d749ff6
MB-teacher <MB-teacher@192.168.1.109>
2020-06-04 19:48:51 +0800

    Java

---
+++ src/main/java/com/study/d21/Car.java
@@ -0,0 +1,26 @@
+package com.study.d21;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+public class Car extends Thread {
+    private CyclicBarrier cb;
+
+    public Car(CyclicBarrier cb) {
+        this.cb = cb;
+    }
+    
+    @Override
+    public void run() {
+        String carName = Thread.currentThread().getName();
+        System.out.printf("%s 台北出發...\n", carName);
+        try {
+            Thread.sleep(new Random().nextInt(5000));
+            System.out.printf("%s 到台中了...\n", carName);
+            cb.await(); // 等待其他車子
+        } catch (Exception e) {
+        }
+        System.out.printf("%s 到高雄了...\n", carName);
+    }
+    
+}
