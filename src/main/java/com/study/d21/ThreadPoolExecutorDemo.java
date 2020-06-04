16706c78ede0220ea9432db265540da9e67c21a2
MB-teacher <MB-teacher@192.168.1.109>
2020-06-04 21:11:34 +0800

    Java

---
+++ src/main/java/com/study/d21/ThreadPoolExecutorDemo.java
@@ -0,0 +1,24 @@
+package com.study.d21;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+// 工作 : 取得幸運數字
+class LuckyNumber implements Runnable {
+    @Override
+    public void run() {
+        System.out.println(new Random().nextInt(100));
+    }
+}
+
+public class ThreadPoolExecutorDemo {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for(int i=1;i<=10;i++) {
+            exec.submit(new LuckyNumber());
+        }
+        System.out.println("執行緒數量: " + Thread.activeCount());
+        exec.shutdown();
+    }
+}
