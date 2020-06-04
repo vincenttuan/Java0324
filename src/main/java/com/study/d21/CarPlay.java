251c404fdea42a2acaad609832771a12264a4631
MB-teacher <MB-teacher@192.168.1.109>
2020-06-04 20:27:55 +0800

    Java

--- src/main/java/com/study/d21/CarPlay.java
+++ src/main/java/com/study/d21/CarPlay.java
@@ -5,7 +5,10 @@ import java.util.concurrent.CyclicBarrier;
 public class CarPlay {
     public static void main(String[] args) {
         int n = 4;
-        CyclicBarrier cb = new CyclicBarrier(n);
+        Runnable r = () -> {
+            System.out.println("我們一起吃太陽餅...");
+        };
+        CyclicBarrier cb = new CyclicBarrier(n, r);
         for(int i=1;i<=n;i++) {
             Car car = new Car(cb);
             car.setName(i + "號車");
