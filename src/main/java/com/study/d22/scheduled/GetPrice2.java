7ef15e92f360392dedf7d61425e2502cd9370636
MB-teacher <MB-teacher@192.168.1.109>
2020-06-09 19:41:28 +0800

    Java

--- src/main/java/com/study/d22/scheduled/GetPrice2.java
+++ src/main/java/com/study/d22/scheduled/GetPrice2.java
@@ -1,5 +1,8 @@
 package com.study.d22.scheduled;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 import java.util.concurrent.Callable;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
@@ -15,10 +18,15 @@ public class GetPrice2 implements Runnable {
     @Override
     public void run() {
         try {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String begin = sdf.format(new Date());
             Stock stock = YahooFinance.get(symbol);
+            int ms = new Random().nextInt(2000);
+            Thread.sleep(ms);
             double price = stock.getQuote().getPrice().doubleValue();
             String name = stock.getName();
-            System.out.printf("symbol: %s(%s) price:%.2f\n", symbol, name, price);
+            String end = sdf.format(new Date());
+            System.out.printf("symbol: %s(%s) price:%.2f 開始時間: %s 查詢時間: %d ms 結束時間: %s\n", symbol, name, price, begin, ms, end);
         } catch (Exception e) {
             
         }
