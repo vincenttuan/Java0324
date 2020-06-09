ec2c2d3abf95f32ae31e55ed12efc2760b607edc
MB-teacher <MB-teacher@192.168.1.109>
2020-06-09 21:41:46 +0800

    Java

--- src/main/java/com/study/d22/forkjoin/Sum.java
+++ src/main/java/com/study/d22/forkjoin/Sum.java
@@ -39,4 +39,6 @@ A. The program prints several values that total 55.
 B. The program prints 55.
 C. A compilation error occurs at line n1.
 D. The program prints several values whose sum exceeds 55.
+Ans : A
+
 */
