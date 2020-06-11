ed185d51a5e315e2b0284c9391d471480e044f00
MB-teacher <MB-teacher@192.168.1.109>
2020-06-11 18:56:51 +0800

    Java

--- src/main/java/com/study/d22/lock/Lotto.java
+++ src/main/java/com/study/d22/lock/Lotto.java
@@ -1,18 +1,26 @@
 package com.study.d22.lock;
 
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Lotto implements Runnable {
     private String name;
+    static ReentrantLock lock = new ReentrantLock();
     public Lotto(String name) {
         this.name = name;
     }
     @Override
     public void run() {
-        for(int i=1;i<=5;i++) {
-            int n = new Random().nextInt(100);
-            System.out.printf("%s 拿第 %d 組的號碼是 : %d\n", name, i, n);
+        try {
+            lock.lock();
+            for(int i=1;i<=5;i++) {
+                int n = new Random().nextInt(100);
+                System.out.printf("%s 拿第 %d 組的號碼是 : %d\n", name, i, n);
+            }
+        } finally {
+            lock.unlock();
         }
+        
     }
     
 }
