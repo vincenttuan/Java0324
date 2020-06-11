142837e24abb0ddfef33a34042fb545cee67771c
MB-teacher <MB-teacher@192.168.1.109>
2020-06-11 20:42:12 +0800

    Java

---
+++ src/main/java/com/study/d23_io/FileWriterDemo.java
@@ -0,0 +1,23 @@
+package com.study.d23_io;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class FileWriterDemo {
+    public static void main(String[] args) {
+        File file = new File("src/main/java/com/study/d23_io/MyFile.txt");
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file, true);
+            fw.write("ABC");
+        } catch (Exception e) {
+        } finally {
+            if(fw != null) {
+                try {
+                    fw.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+    }
+}
