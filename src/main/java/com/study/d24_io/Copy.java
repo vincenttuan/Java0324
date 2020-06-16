d8640705807419e3b69a3a26f62b8b9cd6fe3afe
MB-teacher <MB-teacher@192.168.1.109>
2020-06-16 19:12:52 +0800

    Java

--- src/main/java/com/study/d24_io/Copy.java
+++ src/main/java/com/study/d24_io/Copy.java
@@ -1,10 +1,14 @@
 package com.study.d24_io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Copy {
     public static void main(String[] args) {
@@ -16,17 +20,32 @@ public class Copy {
     
     public void begin(File source, File dest) {
         // 檔案資料暫存區
-        List<String> list = new ArrayList<>();
+        Queue<String> list = new LinkedList<>();
         // 讀取檔案資料
         try(FileReader fr = new FileReader(source);
             BufferedReader br = new BufferedReader(fr);) {
             String line = null;
             while ((line = br.readLine()) != null) {                
                 System.out.println("逐行讀取: " + line);
-                list.add(line);
+                list.offer(line);
             }
         } catch (Exception e) {
         }
+        System.out.println(list);
+        // 逐行寫入
+        try(FileWriter fw = new FileWriter(dest);
+            BufferedWriter bw = new BufferedWriter(fw);) {
+            
+            while (!list.isEmpty()) {                
+                String line = list.poll(); // 資料取出並移除
+                System.out.println("逐行寫入: " + line);
+                bw.write(line);
+                bw.newLine();
+            }
+            
+        } catch (Exception e) {
+        }
+        System.out.println("寫入完成");
         System.out.println(list);
     }
     
