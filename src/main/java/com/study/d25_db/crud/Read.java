b2e688c9bb6dda871c254bea5a2a0453a8f6f8ce
MB-teacher <MB-teacher@192.168.1.109>
2020-06-23 19:49:54 +0800

    Java

--- src/main/java/com/study/d25_db/crud/Read.java
+++ src/main/java/com/study/d25_db/crud/Read.java
@@ -1,5 +1,20 @@
 package com.study.d25_db.crud;
 
+import com.study.d25_db.ConnectionDerby;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Read {
-    
+    public static void main(String[] args) {
+        String sql = "SELECT id, age, name FROM Student";
+        try(Connection conn = ConnectionDerby.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)) {
+            
+            
+            
+        } catch (Exception e) {
+        }
+    }
 }
