237b50ac60f0eb7f1ed4afe0100857eae14d3d85
MB-teacher <MB-teacher@192.168.1.109>
2020-06-23 19:57:23 +0800

    Java

--- src/main/java/com/study/d25_db/crud/Read.java
+++ src/main/java/com/study/d25_db/crud/Read.java
@@ -3,6 +3,7 @@ package com.study.d25_db.crud;
 import com.study.d25_db.ConnectionDerby;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 
 public class Read {
@@ -12,7 +13,15 @@ public class Read {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql)) {
             
-            
+            ResultSetMetaData md = rs.getMetaData();
+            System.out.printf("%5s%5s%10s\n", md.getColumnName(1), md.getColumnName(2), md.getColumnName(3));
+            System.out.println("--------------------");
+            while (rs.next()) {                
+                int id = rs.getInt("id");
+                int age = rs.getInt("age");
+                String name = rs.getString("name");
+                System.out.printf("%5d%5d%10s\n", id, age, name);
+            }
             
         } catch (Exception e) {
         }
