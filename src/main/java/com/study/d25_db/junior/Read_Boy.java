14421f13307ce566edcedfc9c7da2271e004e300
MB-teacher <MB-teacher@192.168.1.109>
2020-06-23 21:32:13 +0800

    Java

---
+++ src/main/java/com/study/d25_db/junior/Read_Boy.java
@@ -0,0 +1,39 @@
+package com.study.d25_db.junior;
+
+import com.study.d25_db.ConnectionDerby;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class Read_Boy {
+
+    public static void main(String[] args) throws Exception {
+        String sql = "SELECT school, quote, score FROM APP.JUNIOREXAM\n" +
+                     "WHERE school not like ('%女%')";
+        try (Connection conn = ConnectionDerby.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery();) {
+            
+            ResultSetMetaData md = rs.getMetaData();
+            System.out.printf("%10s%10s%10s\n", md.getColumnName(1), md.getColumnName(2), md.getColumnName(3));
+            System.out.println("------------------------------");
+            
+            int my_sort = 2500;
+            String my_school = null;
+            int quote_sum = 0;
+            while (rs.next()) {               
+                String school = rs.getString("school");
+                int quote = rs.getInt("quote");
+                quote_sum += quote;
+                if(quote_sum >= my_sort) {
+                    my_school = school;
+                    break;
+                }
+            }
+            System.out.printf("排名: %d 落點學校: %s\n", my_sort, my_school);
+            
+        } catch (Exception e) {
+        }
+    }
+}
