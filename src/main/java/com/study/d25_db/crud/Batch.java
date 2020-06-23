d96ccced80d5d7175f4614ea48d6cc368e47778d
MB-teacher <MB-teacher@192.168.1.109>
2020-06-23 20:53:30 +0800

    Java

--- src/main/java/com/study/d25_db/crud/Batch.java
+++ src/main/java/com/study/d25_db/crud/Batch.java
@@ -1,20 +1,27 @@
 package com.study.d25_db.crud;
 
+import com.github.javafaker.Faker;
 import com.study.d25_db.ConnectionDerby;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Batch {
     public static void main(String[] args) {
         String sql = "INSERT INTO Student(age, name) VALUES(?, ?)";
         try(Connection conn = ConnectionDerby.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            pstmt.clearBatch();
             
-            pstmt.setInt(1, new Random().nextInt(40) + 10);
-            pstmt.setString(2, "Joe");
-            int rowcount = pstmt.executeUpdate();
-            System.out.printf("異動筆數: %d\n", rowcount);
+            for (int i = 0; i < 100; i++) {
+                pstmt.setInt(1, new Random().nextInt(40) + 10);
+                pstmt.setString(2, new Faker().name().firstName());
+                pstmt.addBatch();
+            }
+            
+            int[] rowcounts = pstmt.executeBatch();
+            System.out.printf("異動筆數: %d\n", rowcounts.length);
             
         } catch (Exception e) {
         }
