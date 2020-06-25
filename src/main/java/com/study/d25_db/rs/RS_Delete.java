b2162f9576543d473b7a4902159fe7eb54a1937a
MB-teacher <MB-teacher@192.168.1.109>
2020-06-25 19:20:08 +0800

    Java

---
+++ src/main/java/com/study/d25_db/rs/RS_Delete.java
@@ -0,0 +1,31 @@
+package com.study.d25_db.rs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class RS_Delete {
+    public static void main(String[] args) {
+        String url = "jdbc:derby://localhost:1527/sample";
+        String username = "app";
+        String password = "app";
+        
+        try(Connection conn = DriverManager.getConnection(url, username, password);
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery("SELECT id, age, name FROM Student WHERE name = '屈原'"); ) {
+            
+            // 刪除
+            if(rs.last()) {
+                rs.deleteRow();
+                System.out.println("刪除完成");
+            } else {
+                System.out.println("無此資料");
+            }
+            
+            
+        } catch (Exception e) {
+        }
+        
+    }
+}
